ee01d75290f2a6fd737aef9e8fd922be1a1ac629
AlexandruPiciorus <alexandru.piciorus@nexttech.ro>
2024-07-08 10:43:40 +0300

    final project

--- src/main/java/org/example/GamificationApplication.java
+++ src/main/java/org/example/GamificationApplication.java
@@ -2,10 +2,17 @@ package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication(scanBasePackages = "org.example")
-public class GamificationApplication {
+public class GamificationApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(GamificationApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(GamificationApplication.class);
     }
 }
